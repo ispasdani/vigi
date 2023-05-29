34e8cdb92251295a5675145762cf38c594b71f5b
ispasdani <53304187+ispasdani@users.noreply.github.com>
2023-05-29 20:01:06 +0200

    refactoring

---
+++ components/Banners/PresentationDynamicBanner/PresentationDynamicBanner.tsx
@@ -0,0 +1,5 @@
+import styles from "@vx/components/Banners/PresentationDynamicBanner/styles.module.scss";
+
+export const PresentationDynamicBanner = () => {
+  return <div className={styles.presDynBannner}>Test</div>;
+};
--- components/Buttons/SignInButton/SignInBtn.tsx
+++ components/Buttons/SignInButton/SignInBtn.tsx
@@ -1,7 +1,12 @@
 import styles from "@vx/components/Buttons/SignInButton/signInBtn.module.scss";
+import Link from "next/link";
 
 const SignInBtn = () => {
-  return <div className={styles.signInBtn}>Sign in</div>;
+  return (
+    <div className={styles.signInBtn}>
+      <Link href="/authentification">Sign in</Link>
+    </div>
+  );
 };
 
 export default SignInBtn;
